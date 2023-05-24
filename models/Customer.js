98670db1fd7789b8729141cbd8d6b0811704f91e
Nemuel Wainaina <bR0S3@protonmail.com>
2023-05-24 09:20:16 +0300

    Define the Customer schema

---
+++ models/Customer.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const customerSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email_addr: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Customer', customerSchema)
