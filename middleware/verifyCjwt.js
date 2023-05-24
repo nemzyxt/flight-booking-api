c200387067b8e7a1b1aa98c02a9f2f689605b19f
Nemuel Wainaina <bR0S3@protonmail.com>
2023-05-24 11:46:59 +0300

    Create middleware for customer routes

---
+++ middleware/verifyCjwt.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+
+const verifyCjwt = (req, res, next) => {
+    const token = req.get('x-token')
+    try {
+        const payload = jwt.verify(token, key)
+        req.body.customer_id = payload.customer_id
+        req.body.email_addr = payload.email_addr
+        next()
+    } catch(err) {
+        res.status(401).send('unauthorized :(')
+    }
+}
+
+module.exports = verifyCjwt
